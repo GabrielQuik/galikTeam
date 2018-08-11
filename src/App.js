6065487243d11dc6cc0275e5f378c0d9c3eb0416
Damian Pendrak <dpendrak@gmail.com>
2018-08-11 18:13:50 +0200

    Add link to imageList

--- src/App.js
+++ src/App.js
@@ -7,66 +7,69 @@ import leftContentImage from "../assets/images/M67A6522_1.jpg";
 import rightContentImage from "../assets/images/M67A6522_2.jpg";
 
 class App extends Component {
-  render() {
-    const imageList = [
-      { src: leftContentImage, alt: 'Treningi personalne dla każdego'},
-      { src: rightContentImage, alt: 'Przygotowanie motoryczne sportowców'}
-    ];
-    return (
-      <Fragment>
-        <a className="nav" href="#menu">
-          <svg height="16px" width="27px" version="1.1" viewBox="0 0 27 16">
-            <defs />
-            <g
-              id="Symbols"
-              fill="none"
-              stroke="none"
-              strokeLinecap="square"
-              strokeWidth="1"
-            >
-              <g
-                id="hamburger"
-                stroke="#FFFFFF"
-                strokeWidth="2"
-                transform="translate(-12.000000, -17.000000)"
-              >
-                <path id="Line" d="M13,18 L38,18" />
-                <path id="Line-Copy" d="M13,25 L38,25" />
-                <path id="Line-Copy-2" d="M13,32 L38,32" />
-              </g>
-            </g>
-          </svg>
-          <span id="menu_text">menu</span>
-        </a>
-        <div className="container">
-          {imageList.map(imageData => (
-            <div className="container__half">
-              <img
-                className="half__image"
-                src={imageData.src}
-                alt={imageData.alt}
-              />
-          </div>
-          ))}
-        </div>
-        <div id="toAbout">
-          <svg height="19px" width="109px" version="1.1" viewBox="0 0 109 19">
-            <defs />
-            <g id="desktop" fill="none" stroke="none" strokeWidth="1">
-              <g id="start" transform="translate(-906.000000, -1014.000000)">
-                <polyline
-                  id="Path-3"
-                  points="906.666667 1015.66406 960.442708 1031.48438 1014.21875 1015.66406"
-                  stroke="#FFFFFF"
-                  strokeWidth="2"
-                />
-              </g>
-            </g>
-          </svg>
-        </div>
-      </Fragment>
-    );
-  }
+    render() {
+        const imageList = [
+            { src: leftContentImage, alt: 'Treningi personalne dla każdego', link: '/treningi-personalne' },
+            { src: rightContentImage, alt: 'Przygotowanie motoryczne sportowców', link: '/przygotowanie-motoryczne-sportowcow' }
+        ];
+        return (
+            <Fragment>
+                <a className="nav" href="#menu">
+                    <svg height="16px" width="27px" version="1.1" viewBox="0 0 27 16">
+                        <defs />
+                        <g
+                            id="Symbols"
+                            fill="none"
+                            stroke="none"
+                            strokeLinecap="square"
+                            strokeWidth="1"
+                        >
+                            <g
+                                id="hamburger"
+                                stroke="#FFFFFF"
+                                strokeWidth="2"
+                                transform="translate(-12.000000, -17.000000)"
+                            >
+                                <path id="Line" d="M13,18 L38,18" />
+                                <path id="Line-Copy" d="M13,25 L38,25" />
+                                <path id="Line-Copy-2" d="M13,32 L38,32" />
+                            </g>
+                        </g>
+                    </svg>
+                    <span id="menu_text">menu</span>
+                </a>
+                <div className="container">
+                    {imageList.map(imageData => (
+                        <div className="container__half">
+                            <img
+                                className="half__image"
+                                src={imageData.src}
+                                alt={imageData.alt}
+                            />
+                            <div>
+
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div id="toAbout">
+                    <svg height="19px" width="109px" version="1.1" viewBox="0 0 109 19">
+                        <defs />
+                        <g id="desktop" fill="none" stroke="none" strokeWidth="1">
+                            <g id="start" transform="translate(-906.000000, -1014.000000)">
+                                <polyline
+                                    id="Path-3"
+                                    points="906.666667 1015.66406 960.442708 1031.48438 1014.21875 1015.66406"
+                                    stroke="#FFFFFF"
+                                    strokeWidth="2"
+                                />
+                            </g>
+                        </g>
+                    </svg>
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
