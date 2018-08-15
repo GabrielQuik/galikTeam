4b05a2683a907f24fd5b3065d86e1fedbae5271b
Damian Pendrak <dpendrak@gmail.com>
2018-08-15 20:20:57 +0200

    add about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,11 +3,19 @@ import { Fragment } from 'react';
 import { Link } from 'react-router-dom'
 
 import '../../App.css';
+import '../style/About.css'
+
+import AboutImage from '../../../assets/images/M67A6384_1.jpg'
 
 const About = () => (
     <Fragment>
         <div className="container">
+            <div className="container__half">
+                <img className="half__image" src={AboutImage} />
+            </div>
+            <div className="container__half container__half__text container__half__text--non-transparent">
 
+            </div>
         </div>
     </Fragment>
 
