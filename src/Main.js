7fdfd1b5106710fbe638f675221e16834fee51db
GabrielQuik <GabrielQuik@users.noreply.github.com>
2018-08-21 20:37:10 +0200

    Add transition fade

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Landing from './components/pages/Landing.js'
-import About from './components/pages/About.js'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./Main.css";
+
+import Landing from "./components/pages/Landing.js";
+import About from "./components/pages/About.js";
 
 const Main = () => (
-  <Switch>
-    <Route exact path='/' component={Landing}/>
-    <Route path='/o-mnie' component={About}/>
-  </Switch>
-)
+  <Route
+    render={({ location }) => (
+      <TransitionGroup className="main">
+        <CSSTransition
+          key={location.key}
+          classNames='fade'
+          timeout={300}
+        >
+          <Switch location={location}>
+            <Route exact path="/" component={Landing} />
+            <Route path="/o-mnie" component={About} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    )}
+  />
+);
 
-export default Main
+export default Main;
