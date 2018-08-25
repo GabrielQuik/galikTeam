80825fb62e9fb78aad0604aaa960b096e496f132
Damian Pendrak <dpendrak@gmail.com>
2018-08-25 17:17:38 +0200

    Add ABOUT page;

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,7 +14,19 @@ const About = () => (
                 <img className="half__image" src={AboutImage} />
             </div>
             <div className="container__half container__half__text container__half__text--non-transparent">
-
+                <p>
+                Nazywam się Michał Galikowski i jestem czynnym lekkoatletą specjalizującym się w biegu na 400m przez płotki, trenerem przygotowania motorycznego sportowców oraz trenerem personalnym.
+                <br/>Mimo młodego wieku ze sportem związany jestem od ponad 10 lat. Pochodzę ze sportowej rodziny, dlatego trening i sport to nieodłączna część mojego życia której poświęcam każdy możliwy czas od najmłodszych lat.
+                <br/>Razem z swoim Ojcem i Trenerem - Markiem Galikowskim, absolwentem gdańskiej Akademii Wychowania Fizycznego,  trenerem lekkoatletyki I klasy tworzymy jedną z najskuteczniejszych ekip w Polsce zdobywając przez ostatnie lata sześciokrotnie złoty medal Mistrzostw Polski oraz osiągając wyniki w pierwszej dziesiątce na szczeblu Europejskim. Swoje sukcesy zawdzięczamy przede wszystkim ciężkiej i nieustannej pracy nad sobą.
+                <br/>Przez ostatnie lata będąc członkiem Kadry Polski poznałem metody treningowe wielu wybitnych Trenerów i cały czas jestem głodny wiedzy na temat TRENINGU.
+                <br/>Nie jesteśmy zwykłymi trenerami. Sport i trening to nasze życie. Doświadczenie trenerskie oraz zawodnicze to nasz największy atut.
+                <br/>TO MY. GALIKTEAM.
+                </p>
+                <div className="button">
+                    <a className="button__link" href="/kontakt">
+                        kontakt
+                    </a>
+                </div>
             </div>
         </div>
     </Fragment>
